6e96689cf55ff1a2d0101119d7f451b71e8b1e48
YoungJae Kwon <kyoje11@gmail.com>
2018-04-06 21:31:50 +0900

    first commit

---
+++ src/main/java/com/nesoy/gradle_travis_coveralls/GradleTravisCoverallsApplication.java
@@ -0,0 +1,12 @@
+package com.nesoy.gradle_travis_coveralls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleTravisCoverallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleTravisCoverallsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nesoy/gradle_travis_coveralls/GradleTravisCoverallsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nesoy.gradle_travis_coveralls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GradleTravisCoverallsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
